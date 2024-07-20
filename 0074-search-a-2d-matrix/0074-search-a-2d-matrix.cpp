6d30483cdf64a03716ef11afacb755685166cb70
Riya Kaushik <127042953+kriaaa@users.noreply.github.com>
2024-07-20 15:58:20 +0530

    Time: 7 ms (22.17%), Space: 12.3 MB (6.02%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
@@ -18,17 +18,26 @@ class Solution {
 //           low=mid+1;
 //       }
 //    return false;
-   int rows=matrix.size();
-int columns=matrix[0].size();
-for(int i=0;i<rows;i++)
-{
-    for(int j=0;j<columns;j++)
-    {
-        if(matrix[i][j]==target)
-        return 1;
 
-    }
+int rows=matrix.size();
+int cols=matrix[0].size();
+
+int low=0;
+int high=(rows*cols)-1;
+while(low<=high)
+{
+    int mid=(low+high)/2;
+    int row=mid/cols;
+    int col=mid%cols;
+    if(matrix[row][col]==target)
+    return 1;
+    else if(matrix[row][col]>target)
+    high=mid-1;
+    else
+    low=mid+1;
+
 }
+
 return 0;
   }
 };
