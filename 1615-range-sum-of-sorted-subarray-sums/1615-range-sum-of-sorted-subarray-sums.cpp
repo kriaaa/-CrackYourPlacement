c422b3399d810ac826fe923e6ae3e5b8f7af93b0
Riya Kaushik <127042953+kriaaa@users.noreply.github.com>
2024-08-05 00:44:12 +0530

    Time: 93 ms (48.76%), Space: 42.7 MB (7.87%) - LeetHub

--- 1615-range-sum-of-sorted-subarray-sums/1615-range-sum-of-sorted-subarray-sums.cpp
+++ 1615-range-sum-of-sorted-subarray-sums/1615-range-sum-of-sorted-subarray-sums.cpp
@@ -1,27 +1,33 @@
-
-
 class Solution {
 public:
-    int rangeSum(std::vector<int>& nums, int n, int left, int right) {
-        const int MOD = 1e9 + 7;
-        std::vector<long long> ans;
-        long long sum;
-        
-        for (int i = 0; i < n; i++) {
-            sum = 0;
-            for (int j = i; j < n; j++) {
-                sum += nums[j];
-                ans.push_back(sum);
+    int rangeSum(vector<int>& nums, int n, int left, int right) {
+        const int  modulo=1e9+7;
+
+        vector<long long >ans;
+        long long int sum;
+        for(int i=0;i<n;i++)
+        {
+            sum=0;
+            for(int j=i;j<n;j++)
+            {
+    
+                sum+=nums[j];
+               ans.push_back(sum);
+            
             }
+
+            }
+        
+        int s=ans.size();
+        long long ansnum=0;
+        sort(ans.begin(),ans.end());
+        for(int i=left-1;i<right;i++)
+        {
+            ansnum=(ansnum+ans[i])%modulo;
+
+
         }
-        
-        std::sort(ans.begin(), ans.end());
-        
-        long long ansnum = 0;
-        for (int i = left - 1; i < right; i++) {
-            ansnum = (ansnum + ans[i]) % MOD;
-        }
-        
         return ansnum;
+        
     }
 };
